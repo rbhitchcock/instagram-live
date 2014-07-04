43f51565447682fb08861aba47651ea8e8effa9c
Blake Hitchcock <rbhitchcock@gmail.com>
2014-07-04 19:03:57 -0400

    Basic app using SSE

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+set server: :thin, connections: []
+enable :sessions
+set :haml, format: :html5
+
+get '/stream', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    settings.connections << out
+    out.callback { settings.connections.delete out }
+  end
+end
+
+post '/' do
+  settings.connections.each do |out|
+    out << "data: #{params[:msg]}\n\n"
+  end
+  204
+end
+
+get '/' do
+  halt haml(:login) unless params[:user]
+  haml :chat, locals: { user: params[:user].gsub(/\W/, '') }
+end
