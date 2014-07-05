6fe83a1fde2904a52721aa875d44a4a256e6dd72
Blake Hitchcock <rbhitchcock@gmail.com>
2014-07-05 13:15:13 -0400

    Start using JSX and add assets rake task

---
+++ Rakefile
@@ -0,0 +1,13 @@
+PUBLIC_DIR = "public/"
+namespace :assets do
+  task :precompile do
+    # jsx => js
+    js_dir = PUBLIC_DIR << "js/"
+    `mkdir -p #{js_dir}`
+    Dir["assets/jsx/*.jsx"].each do |jsx|
+      base = jsx.match(%r{/(\w+)\.jsx$})[1]
+      ofile = js_dir << base <<  ".js"
+      `jsx #{jsx} > #{ofile}`
+    end
+  end
+end
--- app.rb
+++ app.rb
@@ -1,39 +1,61 @@
-require 'sinatra'
 set server: :thin, connections: []
 enable :sessions
 set :haml, format: :html5
+$stdout.sync = true
 
 ACCESS_TOKEN = ENV['IG_ACCESS_TOKEN']
 CLIENT_ID = ENV['IG_CLIENT_ID']
 CLIENT_SECRET = ENV['IG_CLIENT_SECRET']
 
-get '/stream', provides: 'text/event-stream' do
-  stream :keep_open do |out|
-    settings.connections << out
-    out.callback { settings.connections.delete out }
+class Instagram
+  include HTTParty
+  format :json
+  base_uri 'api.instagram.com:443'
+  headers 'X-Requested-With' => "XMLHttpRequest", "X-Target-URI" => "https://api.instagram.com"
+
+  def initialize(access_token)
+    @access_token = access_token
+  end
+
+  def tagged_with(tag, options={})
+    self.class.get "/v1/tags/#{tag}/media/recent?access_token=#{@access_token}"
   end
 end
 
-get '/listener' do
-  if params[:"hub.mode"] == "subscribe"
-    params[:"hub.challenge"]
+class Streamer < Sinatra::Application
+  get '/subscribe', provides: 'text/event-stream' do
+    puts params.inspect
+    stream :keep_open do |out|
+      settings.connections << out
+      out.callback { settings.connections.delete out }
+    end
   end
-end
 
-post '/listener' do
-  puts request.inspect
-  puts "BODY: #{request.body.read}"
-  puts "PARAMS: #{JSON.parse(request.body.read)}"
-end
-
-post '/' do
-  settings.connections.each do |out|
-    out << "data: #{params[:msg]}\n\n"
+  get '/somethingelse' do
+    logger.info params.inspect
   end
-  204
-end
 
-get '/' do
-  halt haml(:login) unless params[:user]
-  haml :chat, locals: { user: params[:user].gsub(/\W/, '') }
+  get '/listener' do
+    if params[:"hub.mode"] == "subscribe"
+      params[:"hub.challenge"]
+    end
+  end
+
+  post '/listener' do
+    puts request.inspect
+    puts "BODY: #{request.body.read}"
+    puts "PARAMS: #{JSON.parse(request.body.read)}"
+    settings.connections.each do |out|
+      out << "data: #{params[:msg]}\n\n"
+    end
+    204
+  end
+
+  get '/tag/:tag' do
+    Instagram.new(ACCESS_TOKEN).tagged_with(params[:tag]).body
+  end
+
+  get '/' do
+    haml :index
+  end
 end
