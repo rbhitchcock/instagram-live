e8d1f238aa550690767e0a9ad045289a138fc7e6
Blake Hitchcock <rbhitchcock@gmail.com>
2014-07-06 19:16:14 -0400

    Start trying to track the min_id of a response

--- app.rb
+++ app.rb
@@ -69,6 +69,7 @@ class Streamer < Sinatra::Application
 
   before do
     @client = Instagram.client(access_token: ACCESS_TOKEN)
+    session[:tags] ||= Hash.new { |h, k| h[k] = {} }
   end
 
   get '/subscribe', provides: 'text/event-stream' do
@@ -82,6 +83,7 @@ class Streamer < Sinatra::Application
     fork do
       @client.process_subscription request.body.read, signature: env["HTTP_X_HUB_SIGNATURE"] do |handler|
         handler.on_tag_changed do |tag, data|
+          logger.info(@client.tag_recent_media tag, min_id: session[tag.to_sym][:min_id]).inspect
           logger.info "TAG: #{tag.inspect}"
           logger.info "DATA: #{data.inspect}"
         end
