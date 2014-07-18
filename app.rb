135901973455c064709bb8394da423b1509aefba
Blake Hitchcock <rbhitchcock@gmail.com>
2014-07-18 08:44:44 -0400

    Try configuring access token

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ $stdout.sync = true
 Instagram.configure do |config|
   config.client_id = ENV['IG_CLIENT_ID']
   config.client_secret = ENV['IG_CLIENT_SECRET']
+  config.access_token = ENV['IG_ACCESS_TOKEN']
 end
 ACCESS_TOKEN = ENV['IG_ACCESS_TOKEN']
 VERIFY_TOKEN = "WEEEEEEEE"
