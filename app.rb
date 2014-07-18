374b04b5ce69dfee21c05697ed4036a8def95664
Blake Hitchcock <rbhitchcock@gmail.com>
2014-07-17 23:34:54 -0400

    No verify token

--- app.rb
+++ app.rb
@@ -122,7 +122,7 @@ class Streamer < Sinatra::Application
     case params[:object]
     when "tag"
       tag = params[:tag] || "hammersubscriptiontest"
-      @client.create_subscription verify_token: VERIFY_TOKEN, callback_url: callback, object: "tag", object_id: tag
+      @client.create_subscription callback_url: callback, object: "tag", object_id: tag
     when "geography"
       lat = params[:lat]
       lng = params[:lng]
@@ -130,12 +130,12 @@ class Streamer < Sinatra::Application
       if lat.nil? or lng.nil? or radius.nil?
         raise ArgumentError
       end
-      @client.create_subscription verify_token: VERIFY_TOKEN, callback_url: callback, object: "geography", lat: lat, lng: lng, radius: radius
+      @client.create_subscription callback_url: callback, object: "geography", lat: lat, lng: lng, radius: radius
     when "user"
       # implement later
     else
       tag = params[:tag] || "hammersubscriptiontest"
-      @client.create_subscription verify_token: VERIFY_TOKEN, callback_url: callback, object: "tag", object_id: tag
+      @client.create_subscription callback_url: callback, object: "tag", object_id: tag
     end
   end
 
